333cd2c6d6b7a6a20e6c63a8ba13e254727c9bdf
Mukesh Soni <mukeshsoni@gmail.com>
2017-07-17 20:04:08 +0530

    added a temp folder. added code to execute pulp bulid command

--- src/Main.purs
+++ src/Main.purs
@@ -5,13 +5,18 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Foldable (intercalate)
 import Node.FS.Sync (writeTextFile)
-import Node.Encoding
-import Node.ChildProcess
+import Node.Encoding (Encoding(..))
+import Node.ChildProcess (spawn, defaultSpawnOptions, stdout, stderr, onExit, Exit(..))
+import Node.Stream (onDataString)
+
+moduleDeclaration = "module Main where"
 
 basicPackages = [
     "Prelude"
   , "Control.Monad.Eff.Console (CONSOLE, log)"
   ]
+
+psSrcPath = "temp"
 
 type PackageName = String
 type PackageNameWithImport = String
@@ -22,9 +27,24 @@ appendImport :: PackageName -> PackageNameWithImport
 appendImport = append "import "
 
 psFile :: Array PackageName -> ExprStr -> PsFileContent
-psFile pkgs exprStr = (intercalate "\n" $ map appendImport pkgs) <> "\n\n" <> exprStr
+psFile pkgs exprStr = moduleDeclaration <> "\n\n" <> (intercalate "\n" $ map appendImport pkgs) <> "\n\n" <> exprStr
 
--- writePsFile exprStr = writeTextFile UTF8 "./test.purs" $ psFile basicPackages exprStr
+writePsFile exprStr = writeTextFile UTF8 (psSrcPath <> "/Main.purs") $ psFile basicPackages exprStr
+
+buildPsFile exprStr = do
+  cp <- spawn "pulp" ["build", "--src-path", "temp", "--to", "temp/output.js"] defaultSpawnOptions
+  let stout = stdout cp
+      sterr = stderr cp
+  onDataString stout UTF8 \s ->
+    log s
+
+  onDataString sterr UTF8 \s ->
+    log s
+
+  onExit cp \e -> case e of
+    Normally 0 -> log (show e)
+    Normally code -> log (show code)
+    BySignal _ -> pure unit
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
--- temp/Main.purs
+++ temp/Main.purs
@@ -1,4 +1,7 @@
+module Main where
+
 import Prelude
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-hey there
+main = do
+  log "hey there"
